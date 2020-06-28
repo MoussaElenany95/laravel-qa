a6a66416928adc31abad06f177670ecb8fe0cf73
MoussaElenany <mousa.elenanyfciscu@gmail.com>
2020-06-28 15:13:13 +0200

    Adding user and question details

--- app/Question.php
+++ app/Question.php
@@ -9,12 +9,21 @@ class Question extends Model
 {
     protected $fillable = ['title','body'];
 
+    // one to many relationship between user table
     public  function user(){
-
         return $this->belongsTo(User::class);
     }
+    //set attributes for question
     public function setTitleAttribute($value){
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value,'-');
+    }
+    //get question url
+    public function getUrlAttribute(){
+        return route('questions.show',$this->id);
+    }
+    //get question date
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
     }
 }
