a4d251a6d7956a392f0186aa2079ca3e54e5df92
MoussaElenany <mousa.elenanyfciscu@gmail.com>
2020-06-28 09:49:46 +0200

    Using faker to use fake data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $random = rand(1,5);
+        factory(App\User::class,3)->create()->each(function ($u) use ($random) {
+            $u->questions()
+              ->saveMany(factory(App\Question::class , $random)->make());
+        });
     }
 }
