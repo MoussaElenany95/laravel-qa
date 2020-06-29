bbf345198d5090f02d551115718bfa39e180d20e
MoussaElenany <mousa.elenanyfciscu@gmail.com>
2020-06-29 10:08:11 +0200

    Ask question validation

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -17,14 +17,24 @@
 
                     <div class="card-body">
                         <form action="{{route('questions.store')}}" method="post">
-                            {{csrf_field()}}
+                            @csrf
                             <div class="form-group">
                                 <label for="question-title">Title</label>
-                                <input type="text" name="title" id="question-title" class="form-control">
+                                <input type="text" name="title" value="{{old('title')}}" id="question-title" class="form-control {{$errors->has('title')? 'is-invalid': ''}}">
+                                @if($errors->has('title'))
+                                    <div class="invalid-feedback">
+                                        <strong>{{$errors->first('title')}}</strong>
+                                    </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <label for="question-body">Explain your question</label>
-                                <textarea name="body" rows="10" class="form-control" id="question-body"></textarea>
+                                <textarea name="body" rows="10" class="form-control {{$errors->has('body')? 'is-invalid': ''}}" id="question-body">{{old('body')}}</textarea>
+                                @if($errors->has('body'))
+                                    <div class="invalid-feedback">
+                                        <strong>{{$errors->first('body')}}</strong>
+                                    </div>
+                                @endif
                             </div>
                             <div class="form-group">
                                 <button class="btn btn-outline-primary btn-lg">Ask this question</button>
