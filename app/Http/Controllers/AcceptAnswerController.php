d075bc2accba3dfd0150678d372faf71e5d7a42b
MoussaElenany <mousa.elenanyfciscu@gmail.com>
2020-07-05 21:37:00 +0200

    Authorize accept answer

--- app/Http/Controllers/AcceptAnswerController.php
+++ app/Http/Controllers/AcceptAnswerController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class AcceptAnswerController extends Controller
 {
+    public function __construct()
+    {
+         $this->middleware('auth');
+    }
+
     public function __invoke(Answer $answer)
     {
         $this->authorize('accept',$answer);
